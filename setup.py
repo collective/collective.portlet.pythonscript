7c0a4129631b9c8795ef3b8d03a11d090cb76855
Tomasz Maćkowiak <kurazu@kurazu.net>
2013-05-09 10:34:35 +0200

    init

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+name = "collective.portlet.pythonscript"
+version = "0.1"
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name=name,
+    version=version,
+    description="Python script portlet",
+    long_description=read('README.md'),
+    # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords="",
+    author="",
+    author_email='',
+    url='',
+    license='',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # framework
+        'Plone'
+    ]
+)
