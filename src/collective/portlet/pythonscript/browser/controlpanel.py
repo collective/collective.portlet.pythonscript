c3ef866435d151233e5b78b81f5435b2e47b9a2c
Tomasz Maćkowiak <kurazu@kurazu.net>
2013-05-09 17:07:51 +0200

    listing of scripts

--- src/collective/portlet/pythonscript/browser/controlpanel.py
+++ src/collective/portlet/pythonscript/browser/controlpanel.py
@@ -4,3 +4,18 @@ class PythonScriptControlPanel(BrowserView):
     """
     Form for managing Python Scripts TTW.
     """
+
+    def getScripts(self):
+        """Return list of available scripts descriptions."""
+        return [
+            {
+                'id': u'xxx',
+                'title': u'XXX',
+                'enabled': True
+            },
+            {
+                'id': u'yyy',
+                'title': u'YYY',
+                'enabled': False
+            }
+        ]
