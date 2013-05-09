a6568a0fbf0c657a31de213538436c173333397d
Tomasz Maćkowiak <kurazu@kurazu.net>
2013-05-09 16:22:31 +0200

    control panel view

--- src/collective/portlet/pythonscript/browser/controlpanel.py
+++ src/collective/portlet/pythonscript/browser/controlpanel.py
@@ -1,23 +1,6 @@
-# -*- coding: utf-8 -*-
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.controlpanel.form import ControlPanelForm
-from zope.formlib.form import action, FormFields
-from zope.interface import Interface, implements
+from Products.Five import BrowserView
 
-class IPythonScriptControlPanel(Interface):
-    """Form interface."""
-
-class IPythonScriptControlPanelFields(Interface):
-    """Fields of the form."""
-
-class PythonScriptControlPanel(ControlPanelForm):
+class PythonScriptControlPanel(BrowserView):
     """
     Form for managing Python Scripts TTW.
     """
-    implements(IPythonScriptControlPanel)
-
-    label = u'Manage Python Scripts for use in portlets.'
-    description = u'Here you can add and manage Python Scripts that later-on can be choosen to render a Script Portlet.'
-    id = u'manage-pythonscript'
-    form_name = u''
-    form_fields = FormFields(IPythonScriptControlPanelFields)
