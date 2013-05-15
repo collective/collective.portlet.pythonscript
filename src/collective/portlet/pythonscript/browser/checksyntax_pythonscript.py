bcabc741e1446675e445516cd8572c3e60f27221
Tomasz Maćkowiak <kurazu@kurazu.net>
2013-05-15 11:20:21 +0200

    syntax checking

---
+++ src/collective/portlet/pythonscript/browser/checksyntax_pythonscript.py
@@ -0,0 +1,35 @@
+from collective.portlet.pythonscript.content.scriptmanager import IPythonScriptManager
+from plone.protect import CheckAuthenticator, PostOnly
+from collective.portlet.pythonscript.browser.controlpanel import PythonScriptControlPanel
+
+class CheckSyntaxPythonScriptView(PythonScriptControlPanel):
+    """List scripts for Syntax Errors in all scripts."""
+
+    def __call__(self):
+        """Render."""
+        # Check against CSRF.
+        CheckAuthenticator(self.request)
+        PostOnly(self.request)
+
+        return super(CheckSyntaxPythonScriptView, self).__call__()
+
+    def checkSyntax(self, script):
+        """Check syntax of given Python Script.
+
+        Returns sequence of error messages or empty tuple if no error is found.
+        """
+        script._compile()
+        errors = script.errors
+        return errors
+
+    def getScripts(self):
+        """Return list of available scripts descriptions."""
+        manager = IPythonScriptManager(self.context)
+        for path, info in manager.getScripts():
+            script = manager.getScript(path)
+            yield {
+                'path': path,
+                'title': info.title,
+                'enabled': info.enabled,
+                'errors': self.checkSyntax(script)
+            }
