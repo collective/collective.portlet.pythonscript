eb2004f3283a6a2e23a34783924707fbb0dd80f9
hvelarde <hector.velarde@gmail.com>
2013-05-29 14:26:21 -0300

    fix package MANIFEST.in

--- setup.py
+++ setup.py
@@ -18,36 +18,40 @@ setup(name='collective.portlet.pythonscript',
       version=version,
       description="Portlet rendering collection of items returned by customizable Python scripts",
       long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.1",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Tomasz Mackowiak',
       author_email='tomasz.mackowiak@stxnext.pl',
-      url='https://github.com/stxnext/collective.portlet.pythonscript',
-      license='gpl',
+      url='https://github.com/collective/collective.portlet.pythonscript',
+      license='GPLv2',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['collective', 'collective.portlet'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
           'Plone',
-          'plone.app.testing'
+          'setuptools',
       ],
-
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ],
+      },
       entry_points="""
       # -*- Entry points: -*-
-        [z3c.autoinclude.plugin]
-        target = plone
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
