5111a5a45d9fdc42dfffd967a424211af3ab44e2
Tomasz Maćkowiak <kurazu@kurazu.net>
2013-06-26 17:06:25 +0200

    docstrings added

--- src/collective/portlet/pythonscript/content/event.py
+++ src/collective/portlet/pythonscript/content/event.py
@@ -3,11 +3,13 @@ from Products.CMFCore.utils import getToolByName
 from collective.portlet.pythonscript.content.interface import IPythonScriptManager
 
 def onPythonScriptAdded(script):
+    """Handle adding Python script item - register it with script manager."""
     portal_url = getToolByName(script, 'portal_url')
     plone_site = portal_url.getPortalObject()
     manager = IPythonScriptManager(plone_site)
     manager.addScript(script)
 
 def onObjectAdded(obj, event):
+    """PythonScripts don't implement any interfaces, so we need to check with isinstance."""
     if isinstance(obj, PythonScript):
         onPythonScriptAdded(obj)
